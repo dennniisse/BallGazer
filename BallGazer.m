70e317179678827895b55b4cc8704155156b0913
ashwincoburn <ashwincoburn@gmail.com>
2022-09-20 21:33:43 +1000

    Create BallGazer.m

---
+++ BallGazer.m
@@ -0,0 +1,11 @@
+%% 41014 SPR2022 - Ball Gazers
+%  Ashwin COBURN (12963690)
+%  Denisse FERNANDEZ (xxxxxxxx)
+%  Hee CHAN-KWON (xxxxxxxx)
+
+close all
+clf
+clc
+hold on;
+% set(0,'DefaultFigureWindowStyle','docked');
+
