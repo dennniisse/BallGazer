92996c33abbb394f8160efff5911f26c86207a02
dennniisse <fernandezdenisse0605@gmail.com>
2022-10-05 19:58:48 +1100

    Integrate all thre classes into one

--- @UR3/UR3.m
+++ @UR3/UR3.m
@@ -8,6 +8,48 @@ classdef UR3 < handle
         workspace = [-0.6 0.6 -0.6 0.6 0 0.8];
         steps = 50;
         baseUR3 = eye(4);
+        
+        %%%%%%%%%%%%%%%%%%% Move it function
+        %>Robot Models
+        model_Arm1; model_EE1; model_Arm2; model_EE2;
+        
+        %>Gripper Mode ['Open' 'Close' 'Ignore']
+%         expectedgripmode = {'Open','Close','Ignore'};
+        gripModeEE1 = 'Ignore'; gripModeEE2 = 'Ignore'; %default
+
+        %>Initial Joint Angles
+        q1_Arm1; q1_Arm2;
+
+        %>Final Transform
+        T2_Arm1; T2_Arm2;
+
+        %>Interpolation Method ['jtraj','ctraj','TVP']
+%         expectedInterp = {'jtraj','ctraj','TVP'};
+        interpMethod_Arm1 = 'ctraj'; %default
+        interpMethod_EE1 = 'ctraj'; 
+        interpMethod_Arm2 = 'ctraj'; 
+        interpMethod_EE2 = 'ctraj';
+
+%         %>Animation Steps
+        steps_Arm1=100; steps_EE1=100; steps_Arm2=100; steps_EE2=100;
+% 
+%         %>Animation Pause
+        animationPause = 0.02;
+
+        %>Final Joint Values Storage (For Next Movement)
+        qMatrixFinal_Arm1 = 0;
+        qMatrixFinal_EE1 = 0;
+        qMatrixFinal_Arm2 = 0;
+        qMatrixFinal_EE2 = 0;
+
+        %>Decision Variables
+        isthere_EE1 = false; %move End Effector for Arm 1
+        isthere_Arm2 = false; %move Arm 2
+        isthere_EE2 = false; %move End Effector for Arm 2
+
+        %> workspace
+        workspace = [-0.6 0.6 -0.6 0.6 -0.2 1.1];   
+        
     end
     
     methods
@@ -45,10 +87,8 @@ classdef UR3 < handle
             
             
             self.manipulator = SerialLink(L,'name',name);
-        end
-        %% PlotAndColourRobot
-        function PlotAndColourRobot(self)
-            % Obtain 3D model of UR3
+            
+             % Obtain 3D model of UR3
             for linkIndex = 1:self.manipulator.n
                 [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['ur3_',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
                 self.manipulator.faces{linkIndex + 1} = faceData;
@@ -75,8 +115,8 @@ classdef UR3 < handle
                     continue;
                 end
             end
-            
         end
+
         %% Plot and Colour Racket
         function GetRacketEE(self)
             % Create links
@@ -122,7 +162,34 @@ classdef UR3 < handle
         end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Move Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function Move(self)
+        function qMatrixFinal_Arm1 = OneArmT2(model_Arm1,model_EE1,q1_Arm1,T2_Arm1,interpMethod_Arm1,steps_Arm1,animationPause)
+            
+            T1_Arm1 = model_Arm1.model.fkine(q1_Arm1);        
+            q2_Arm1 = model_Arm1.model.ikcon(T2_Arm1,q1_Arm1); %Consider joint limits and initial joint angles
+
+            if interpMethod_Arm1 == 1
+                qMatrixArm1 = jtraj(q1_Arm1,q2_Arm1,steps_Arm1); %jtraj
+            elseif interpMethod_Arm1 == 2
+                trajectory = ctraj(T1_Arm1,T2_Arm1,steps_Arm1); %ctraj pt1
+                qMatrixArm1 = model_Arm1.model.ikcon(trajectory,q1_Arm1); %ctraj pt2
+            elseif interpMethod_Arm1 == 3
+                qMatrixArm1 = TVP(q1_Arm1,q2_Arm1,steps_Arm1); %TVP
+            else
+                disp('Invalid interpMethod!');
+            end
+            
+            %Plot & Update Robots
+            disp('Moving Arm...');
+            for i = 1:1:steps_Arm1
+                EE_pos = qMatrixArm1(i,:);
+                model_Arm1.model.animate(qMatrixArm1(i,:)); %Animate Arm1
+                model_EE1.update_gripper(model_Arm1.model.fkine(EE_pos)); %Update and Animate EE1
+                pause(animationPause);
+            end
+
+            qMatrixFinal_Arm1 = qMatrixArm1(steps_Arm1,:); %Remember Last Q
+            
+            disp('Complete!');
         end
     end
 end
