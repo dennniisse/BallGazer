da31a74f82d44f3cac75e38367d9156318067b4a
dennniisse <fernandezdenisse0605@gmail.com>
2022-10-18 22:44:12 +1100

    can hit ball

--- Test/movementTest.m
+++ Test/movementTest.m
@@ -1,8 +1,9 @@
 close all; clf; clc; clear;
 base = eye(4);
-PlaceObject("environment_SnC.ply",[-0.4 -0.4 1.5]);
+PlaceObject("Red_Ball.ply",[0 -0.3 1.5]);
 ur3 = UR3(base);
 move = Move();
 q1 = ur3.model.getpos;
-T2 = transl([-0.4 -0.4 1.5])*trotx(pi/2)*trotz(pi/2);
+gripperOffset = 0.2;
+T2 = transl([0 -0.25 1.3])%*trotx(pi/2)*trotz(pi/2);
 move.OneArm_T2(ur3,q1,T2,2,50,0);
