1cb6f061505595f2fdaa4d34c2da4393c0f5dedd
ashwincoburn <ashwincoburn@gmail.com>
2022-10-25 16:36:04 +1100

    Updated Environment & Height value
    Environment: Added safety features and extras, rotated 90 degrees for first load orientation, decimated for optimal load time and visual quality

--- BallGazer.m
+++ BallGazer.m
@@ -12,12 +12,12 @@ hold on;
 disp('ENVIRONMENT SPAWNING...');
 PlaceObject("environment_SnC.ply",[0,0,0]);
 % disp('ENVIRONMENT SPAWNING...');
-test = UR3([0,0,0.9397]);
-% PlaceObject("Red_Ball.ply", [0.15 -0.4 0.9267]);
-% PlaceObject("Blue_Ball.ply", [-0.15 -0.4 0.9267]);
-PlaceObject("Red_Ball.ply", [-0.25 -1 0.9267]);
-PlaceObject("Blue_Ball.ply", [-0.25 -1.05 0.9267]);
+test = UR3([0,0,0.9397+0.0929]);
+PlaceObject("Red_Ball.ply", [0.15 -0.4 0.9267]);
+PlaceObject("Blue_Ball.ply", [-0.15 -0.4 0.9267]);
+PlaceObject("Red_Ball.ply", [-0.25 -1 0.9267+0.0929]);
+PlaceObject("Blue_Ball.ply", [-0.25 -1.05 0.9267+0.0929]);
 
-ball.uploadCorrectColour(s.getColour)
+% ball.uploadCorrectColour(s.getColour)
 
-test.model.teach();
+% test.model.teach();
